4c8a2f2f0a1ee2cc735a0088766a64e8c4a08d1a
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-07-20 17:06:02 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+import Dom from './modules/constructors.js'; //selecionar elementos
+import scriptsHome from './modules/scriptsPaginas/home.js';
+
+
+// PAGES 
+const pageHome = new Dom().el("#page__home")
+
+if (pageHome) { // ★ HOME  
+    new Dom().bodyClass("body__home");
+    scriptsHome();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("dcl");
+});
